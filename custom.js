8ccc2bed79f1a34978fcb8c83c890db4a9942059
Chad Payne <chad@bird-dog.ca>
2010-06-07 15:05:44 -0600

    enabled new style of trim hover

--- custom.js
+++ custom.js
@@ -489,43 +489,7 @@ jQuery(function($) {
 	
 /* QUICKFIND SHOWCASE */	
  if($("#slideout-showcase").length){
-	$('#slideout-showcase .showcase-pane').css({overflow:'hidden'});
-	var pane = $('#slideout-showcase .showcase-pane');
-	var panewidth = pane.width() + 100;													 ;
-	var paneheight = pane.height() + 100;
-	var vehicleImg = $("#slideout-showcase .vehicle img");
-	var vehicleImgW = vehicleImg.width();
-	var vehicleImgH= vehicleImg.height();
-
-    $("#slideout-showcase .vehicle").hover(function (e) {
-		$(this).parent().append('<div id="jquery-overlay"></div>');
-		$('#jquery-overlay').css({position:'absolute',cursor:'pointer',zIndex:200,background:'#eee',opacity:0.65,width:panewidth,height:paneheight});
-		
-		$(this).css({'z-index': 210}).find("img").stop().animate({														 
-																width: 2*vehicleImgW, 
-																height: 2*vehicleImgH,
-															   },{duration: 75});
-		
-		var ulWidth = $(this).parent().width();
-		var thisOffset  = $(this).offset();
-		var diff = ulWidth - thisOffset.left;
-		
-		if (diff >= 100){
-			$(this).find(".trims").css({'z-index': 211 }).stop().addClass('pright').show().css({left: '120px'});
-		} else {
-			$(this).find(".trims").css({'z-index': 211 }).stop().addClass('pleft').show().css({right: '70px'});
-		}
-
-    },function() {
-		$(this).parent().find("#jquery-overlay").remove();
-          $(this).css({'z-index': 0}).find("img").stop().animate({													 
-																	width: vehicleImgW, 
-																	height:vehicleImgH
-																 },{duration: 75}, function() {
-																		$(this).parent().css('z-index', 1); 
-																 });
-          $(this).find('.trims').hide();
-    });
+	$("#slideout-showcase .vehicle").hoverpulse();
   }
 
 
@@ -856,3 +820,81 @@ function addLoadEvent(func)
 
 }
 addLoadEvent(initLightbox);	// run initLightbox onLoad
+
+/*
+ * jQuery HoverPulse Plugin by M. Alsup
+ * Examples and docs at: http://malsup.com/jquery/hoverpulse/
+ * Dual licensed under the MIT and GPL
+ * Requires: jQuery v1.2.6 or later
+ * @version: 1.01  26-FEB-2009
+ */
+
+(function($) {
+
+$.fn.hoverpulse = function(options) {
+    // in 1.3+ we can fix mistakes with the ready state
+    if (this.length == 0) {
+        if (!$.isReady && this.selector) {
+            var s = this.selector, c = this.context;
+            $(function() {
+                $(s,c).hoverpulse(options);
+            });
+        }
+        return this;
+    }    
+    
+	var opts = $.extend({}, $.fn.hoverpulse.defaults, options);
+
+	// parent must be relatively positioned
+	this.parent().css({ position: 'relative' });
+	// pulsing element must be absolutely positioned
+	this.css({ position: 'absolute', top: 0, left: 0 });
+
+	this.each(function() {
+		var $this = $(this);
+		var w = $this.width(), h = $this.height();
+		$this.data('hoverpulse.size', { w: parseInt(w), h: parseInt(h) });
+	});
+
+	// bind hover event for behavior
+	return this.hover(
+		// hover over
+		function() {
+			var $this = $(this);
+			$this.parent().css('z-index', opts.zIndexActive);
+			
+			var size = $this.data('hoverpulse.size');
+			var w = size.w, h = size.h;
+			$this.stop().animate({ 
+				top:  ('-'+opts.size+'px'), 
+				left: ('-'+opts.size+'px'), 
+				height: (h+2*opts.size)+'px', 
+				width:	(w+2*opts.size)+'px' 
+			}, opts.speed);
+		},
+		// hover out
+		function() {
+			var $this = $(this);
+			var size = $this.data('hoverpulse.size');
+			var w = size.w, h = size.h;
+			
+			$this.stop().animate({ 
+				top:  0, 
+				left: 0, 
+				height: (h+'px'), 
+				width:	(w+'px') 
+			}, opts.speed, function() {
+				$this.parent().css('z-index', opts.zIndexNormal);
+			});
+		}
+	);
+};
+
+$.fn.hoverpulse.defaults = {
+	size:  20,
+	speed: 200,
+	zIndexActive: 100,
+	zIndexNormal: 1
+};
+
+})(jQuery);
